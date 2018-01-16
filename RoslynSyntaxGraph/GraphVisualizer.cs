9e2af5d059dbf89bf04ab055bb46aba6361184a2
David Oliver <david.oliver@nventive.com>
2018-01-16 11:16:33 -0500

    Add visualization
    Parse nodes to simple GraphViz string.

---
+++ RoslynSyntaxGraph/GraphVisualizer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoslynSyntaxGraph
+{
+	public static class GraphVisualizer
+	{
+		/// <summary>
+		/// Convert graph to GraphViz string.
+		/// </summary>
+		public static string ToGraphViz(IEnumerable<GraphNode> graph)
+		{
+			var sb = new StringBuilder();
+			sb.AppendLine("digraph G {");
+
+			foreach (var node in graph)
+			{
+				foreach (var tuple in node.Parents)
+				{
+					sb.AppendLine($"	\"{tuple.Parent.Name}\" -> \"{node.Node.Name}\"");
+				}
+			}
+
+			sb.AppendLine("};");
+
+			return sb.ToString();
+		}
+	}
+}
--- RoslynSyntaxGraph/Program.cs
+++ RoslynSyntaxGraph/Program.cs
@@ -3,19 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace RoslynSyntaxGraph
 {
 	class Program
 	{
+		[STAThread]
 		static void Main(string[] args)
 		{
 			//const string targetSolution = @"C:\src\djo\tests\ConsoleTestbed\ConsoleTestbed.sln";
-			const string targetSolution = @"C:\src\nv\ClubRunner\ClubRunner.sln";
+			//const string targetSolution = @"C:\src\nv\ClubRunner\ClubRunner.sln";
+			const string targetSolution = @"C:\src\nv\Umbrella\Umbrella.sln";
 
 			var wrapper = new GraphBuilder(targetSolution);
-			var nodes = wrapper.BuildGraph().ToArray();
+			var graph = wrapper.BuildGraph().ToArray();
+			//var multiParents = graph.Where(n => n.Parents.Count() > 1).ToArray();
+			//var multiOccurrence = graph.Where(n => n.Parents.FirstOrDefault().Occurrences > 1).ToArray();
+			;
+			var viz = GraphVisualizer.ToGraphViz(graph);
+
+			Console.WriteLine("");
+			Console.WriteLine(viz);
+
+			try
+			{
+
+				Clipboard.SetText(viz);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine($"Clipboard failed: {e}");
+			}
+			Console.ReadLine();
 			;
 		}
+
 	}
 }
