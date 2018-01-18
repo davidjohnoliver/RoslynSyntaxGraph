ae31555ce18e2ca7879bc6db2e86dcfd0aa7a45e
David Oliver <david.oliver@nventive.com>
2018-01-18 15:32:26 -0500

    Output graph to image
    Use GraphVizWrapper to generate image from graph string.

--- RoslynSyntaxGraph/GraphVisualizer.cs
+++ RoslynSyntaxGraph/GraphVisualizer.cs
@@ -1,13 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GraphVizWrapper;
+using GraphVizWrapper.Commands;
+using GraphVizWrapper.Queries;
 
 namespace RoslynSyntaxGraph
 {
-	public static class GraphVisualizer
+	public class GraphVisualizer
 	{
+		public GraphVisualizer(string vizGraph, string graphVizPath, string defaultOutputFolder = "", string graphName = "")
+		{
+			VizGraph = vizGraph;
+			GraphVizPath = graphVizPath;
+			DefaultOutputFolder = defaultOutputFolder;
+			GraphName = graphName;
+		}
+
+		public string VizGraph { get; }
+		public string GraphVizPath { get; }
+		public string DefaultOutputFolder { get; }
+		public string GraphName { get; }
+
+		public void OutputToImageFile(Enums.RenderingEngine renderingEngine, string filePath = null, Enums.GraphReturnType format = Enums.GraphReturnType.Jpg)
+		{
+			filePath = filePath ?? GetFilePath(renderingEngine, format);
+			Console.WriteLine($"Generating {format} with rendering engine {renderingEngine}...");
+			var generator = GetGraphGeneration();
+			generator.RenderingEngine = renderingEngine;
+
+			var img = generator.GenerateGraph(VizGraph, format);
+
+			Console.WriteLine($"Saving {format} to {filePath} ...");
+
+			if (!string.IsNullOrEmpty(DefaultOutputFolder))
+			{
+				Directory.CreateDirectory(DefaultOutputFolder);
+			}
+
+			File.WriteAllBytes(filePath, img);
+		}
+
+		private string GetFilePath(Enums.RenderingEngine renderingEngine, Enums.GraphReturnType format)
+		{
+			// TODO: this only works for some of the formats
+
+			return Path.Combine(DefaultOutputFolder,
+				GraphName + "_" +
+				renderingEngine.ToString().ToLowerInvariant() + "." +
+				format.ToString().ToLowerInvariant());
+		}
+
+		private GraphGeneration GetGraphGeneration()
+		{
+			var startProcessQuery = new GetStartProcessQuery();
+			var getProcessStartInfoQuery = new GetProcessStartInfoQuery();
+			GraphGeneration generation = new GraphGeneration(startProcessQuery, getProcessStartInfoQuery, new RegisterLayoutPluginCommand(getProcessStartInfoQuery, startProcessQuery));
+			generation.GraphvizPath = GraphVizPath;
+
+			return generation;
+		}
+
 		/// <summary>
 		/// Convert graph to GraphViz string.
 		/// </summary>
--- RoslynSyntaxGraph/Program.cs
+++ RoslynSyntaxGraph/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GraphVizWrapper;
 
 namespace RoslynSyntaxGraph
 {
@@ -12,9 +14,11 @@ namespace RoslynSyntaxGraph
 		[STAThread]
 		static void Main(string[] args)
 		{
-			//const string targetSolution = @"C:\src\djo\tests\ConsoleTestbed\ConsoleTestbed.sln";
+			var today = DateTimeOffset.Now.ToString("yyyyMMdd");
+
+			const string targetSolution = @"C:\src\djo\tests\ConsoleTestbed\ConsoleTestbed.sln";
 			//const string targetSolution = @"C:\src\nv\ClubRunner\ClubRunner.sln";
-			const string targetSolution = @"C:\src\nv\Umbrella\Umbrella.sln";
+			//const string targetSolution = @"C:\src\nv\Umbrella\Umbrella.sln";
 
 			var wrapper = new GraphBuilder(targetSolution);
 			var graph = wrapper.BuildGraph().ToArray();
@@ -26,15 +30,29 @@ namespace RoslynSyntaxGraph
 			Console.WriteLine("");
 			Console.WriteLine(viz);
 
+			var graphName = targetSolution.Split('\\').Last();
+			const string baseOutputPath = @"C:\Users\david.oliver\Documents\Work\Internal\RoslynSyntaxGraph";
+			var outputPath = Path.Combine(baseOutputPath, today);
+			const string graphVizPath = @"C:\GraphViz2.38\bin\";
+			var outputter = new GraphVisualizer(viz, graphVizPath, outputPath, graphName);
+			
+			outputter.OutputToImageFile(Enums.RenderingEngine.Dot);
+			//foreach (var engine in Enum.GetValues(typeof(Enums.RenderingEngine)).Cast<Enums.RenderingEngine>())
+			//{
+			//	outputter.OutputToImageFile(engine);
+			//}
+
 			try
 			{
-
 				Clipboard.SetText(viz);
 			}
 			catch (Exception e)
 			{
 				Console.WriteLine($"Clipboard failed: {e}");
 			}
+
+			Console.WriteLine();
+			Console.WriteLine("Done");
 			Console.ReadLine();
 			;
 		}
